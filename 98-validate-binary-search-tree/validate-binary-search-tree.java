23e057f4aaa92f4accb3fa3dba135991b9c0e509
Bhoomi Gupta <bhoomigupta464@gmail.com>
2026-08-26 12:03:55 +0530

    Time: 1 ms (16.45%) | Memory: 46.6 MB (5.02%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,45 +14,27 @@
  * }
  */
 class Solution {
-
     public boolean isValidBST(TreeNode root) {
-
         BSTpair ans = check(root);
-
         return ans.isbst;
     }
-
     public BSTpair check(TreeNode root) {
-
-        // Empty tree is a valid BST
         if (root == null) {
             return new BSTpair();
         }
-
-        // Get information from left and right subtree
         BSTpair left = check(root.left);
         BSTpair right = check(root.right);
-
         BSTpair self = new BSTpair();
-
-        // Check if left or right subtree is already invalid
         if (!left.isbst || !right.isbst) {
             self.isbst = false;
             return self;
         }
-
-        // Check BST property
         if (root.val <= left.max || root.val >= right.min) {
             self.isbst = false;
             return self;
         }
-
-        // Calculate min
         self.min = Math.min(root.val, Math.min(left.min, right.min));
-
-        // Calculate max
         self.max = Math.max(root.val, Math.max(left.max, right.max));
-
         return self;
     }
 }
