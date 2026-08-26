47ab583ffa57dcda14fd06072fa9736f4238136b
Bhoomi Gupta <bhoomigupta464@gmail.com>
2026-08-26 11:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (18.35%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(root.val>key){
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+    // both child exist then there are two method -> (taking maximum of right subtree and move there or taking minimum of left sub tree and move there)
+            else{
+                int max = max(root.left);
+                root.left = deleteNode(root.left,max);
+                root.val = max;
+            }
+        }
+        return root;
+        // for deletion -> three method (leaf node, single child, both child)
+    }
+    public int max(TreeNode root){ // Maximum of BinarySearchTree
+        if(root==null){
+            return Integer.MIN_VALUE;
+        }
+        int rmax = max(root.right);
+        return Math.max(rmax,root.val);
+    }
+}
