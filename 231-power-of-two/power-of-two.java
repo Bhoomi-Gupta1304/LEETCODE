b3edf546d4828fcf6e7dc1a17f9a70e4da76c1bb
Bhoomi Gupta <bhoomigupta464@gmail.com>
2026-09-08 21:21:12 +0530

    Time: 1 ms (96.66%) | Memory: 42.8 MB (18.19%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -6,10 +6,7 @@ class Solution {
         if(n==1){
             return true;
         }
-        if(n==0){
-            return false;
-        }
-        if(n%2!=0){
+        if(n%2!=0 || n==0){
             return false;
         }
         return is(n/2);
