60cb46a903d120f44d182c3faf6b0b1a10715dec
Bhoomi Gupta <bhoomigupta464@gmail.com>
2026-09-11 21:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (34.11%) - LeetSync

--- 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.java
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.java
@@ -1,40 +1,18 @@
 class Solution {
     public int largestInteger(int n, int s) {
-        if(s==0) return 0;
-        if(n*9 < s || s>45 ) return -1;
-        if(n==1 && s>9) return -1;
-        // if(s<10 && n>1) return s * (int)(Math.pow(10,n-1));
-        // if(9*n-s < 10) return nn(n) - (9*n - s);
-        return num(n,s);
-    }
-    public int num(int n,int s){
-        int y=0;
-        while(n>0){
-            int z=s-sum(y);
-            if(z>=9){
-                y= y*10 + 9;
+        if (s > 9 * n) return -1;
+        if (s == 0) return 0;
+        int ans = 0;
+        while (n-- > 0) {
+            if (s >= 9) {
+                ans += 9;
+                s -= 9;
+            } else {
+                ans += s;
+                s = 0;
             }
-            else{
-                y = y*10 + z;
-            }
-            n--;
+            ans *= 10;
         }
-        return y;
-    }
-    public int sum(int n){
-        int s = 0 ;
-        while(n>0){
-            s  += n%10;
-            n/=10;
-        }
-        return s;
-    }
-    public int nn(int n){
-        int s = 0 ;
-        while(n>0){
-            s = s*10 + 9;
-            n--;
-        }
-        return s;
+        return ans / 10;
     }
 }
