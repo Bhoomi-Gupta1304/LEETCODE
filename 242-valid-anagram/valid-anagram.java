a22fa10a080ce2a2bf79a3fa0c7d955215bb9131
Bhoomi Gupta <bhoomigupta464@gmail.com>
2026-09-16 21:21:25 +0530

    Time: 4 ms (83.81%) | Memory: 44.1 MB (89.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
         int [] f = new int[26];
         for(int i=0;i < s.length();i++){
             f[(s.charAt(i))-'a']++;
